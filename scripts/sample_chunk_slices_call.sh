8ed8ae765a5e468029a39c714d0993eb2cf76e97
Jonathan Perrie <jonathan.perrie@gmail.com>
2022-02-28 20:15:55 -0800

    sample args

---
+++ scripts/sample_chunk_slices_call.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python chunk_slices.py -g "ACA" "AUD" -r "/bigstore/GeneralStorage/jperrie/probe_design/data/slice/sm2/" -w "/bigstore/GeneralStorage/jperrie/probe_design/data/batch_data/" -d "sm2"
